dbc86ee60ea3720732607c98affb3f08ee0335b3
Maria Jose Alcaraz <marialcaraz.192@gmail.com>
2025-05-11 12:40:46 +0200

    Mejoras de CSS

---
+++ cinevelvet-frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cinevelvet',
+  appName: 'CinemaVelvet',
+  webDir: 'dist'
+};
+
+export default config;
